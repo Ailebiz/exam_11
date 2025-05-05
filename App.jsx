afa6192b063bcd243fc78031c03645ec7c1e250f
Ailebiz <bolatkyzyaliza@gmail.com>
2025-05-05 17:13:02 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import AiImageGenerator from "./Conponents/AiImageGenerator";
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex justify-center items-center p-6">
+
+      <AiImageGenerator />
+    </div>
+  );
+}
+
+export default App;
+
